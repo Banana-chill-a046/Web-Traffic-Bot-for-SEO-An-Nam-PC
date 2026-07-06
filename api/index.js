a17c9b9b86b40335eadddfca198b604add938d46
Vu Hoang An Nam ( annampc ) <vuhoangannam04@gmail.com>
2026-07-06 21:23:11 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
     websites: [
       "https://www.annampc.io.vn",
       "https://nammc.nammc.click",
-      "https://testpc.nammc.click",
+      "https://testpc.annampc.io.vn",
       "https://www.hoanghaopc.com"
     ],
     time: new Date().toISOString()
